08a03b7ac33981023877bdb5f9134acad9c5423a
Mireya Josebeth García Rodríguez <151254789+mireya111@users.noreply.github.com>
2024-10-24 15:36:15 -0500

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,34 @@
+//Sprint 0
+/*
+- Delimitar los requisitos a cumplir 
+( Hasta deonde se realizará el proyecto, rutas públicas y privadas )
+( El backend p0ermite el registro e inicio de sesión de usuarios administradores)
+( El backend permite gestionar tours )
+( El backend permite viasualizar y contratar tours )
+( Figura más descripción )
+Nota.- Delimitar cada temática, ¿Qué hace el administrador y el cliente (los roles que se presentan)?
+
+- Estructuración general de las carpetas del proyecto 
+¿Qué herramientaa se utilizan?
+VSCODE + JS + EXPRESS + ARCHIVOS Y DIRECTORIOS 
+
+- Diseño de las colecciones de datos para MongoDB
+BDD              TABLAS                  REGISTROS 
+NSQL             COLECCIONES             DOCUMENTOS 
+ANALOGÍA: UNA CARPETA QUE TIENE HOJAS 
+
+2 COLECCIOENS: 
+JSON - Server
+npx json-server --watch db.json --port=4000 => corre la base de datos en otro puerto 
+
+Administrador o conductor 
+- Gestionar tours
+Cliente 
+- Visualizar y contratarun tour 
+
+Levantamiento de el servidor  npm run dev
+
+1. Modelo 
+2. Controlador 
+3.
+*/ 
